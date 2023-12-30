2331ef7ad75cc461ce7793cfa607d3bd149022e9
@NathEnej <101989879+nathaneneja@users.noreply.github.com>
2023-12-30 14:01:23 +0100

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>My Favorite Foods</h1>
+    <ul>
+      <li>Bacon</li>
+      <li>Jamon</li>
+      <li>Noodles</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
